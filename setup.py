e290de1d9b3d772c8809845f0123b78ac5ec3988
Junior <rizkyputrajunior@gmail.com>
2024-07-31 16:53:10 +0700

    chore: add new dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="carbon",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "urwid",
         "kubernetes",
         "PyYAML"
+        "ptyprocess"
+        "pyte"
     ],
     entry_points={
         'console_scripts': [
